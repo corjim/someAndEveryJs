c689421578b4bb21e7420603201d070ac246126d
MartinOrji <martin.orji.jnr@gmail.com>
2024-06-17 01:13:49 -0500

    add find & findIndex solutions

--- app.js
+++ app.js
@@ -52,3 +52,26 @@ function hasCertainValue(str, key, value) {
         return val[key] === value;
     })
 };
+
+
+//  SOLUTIONS FOR FIND / FINDINDEX
+
+
+// Write a function called findUserByUsername which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined.
+
+function findUserByUsername(arr, username) {
+    return arr.find(function (el) {
+        return el.user === username;
+    })
+};
+
+//  Write a function called removeUser which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array and return this object. If the object is not found, return undefined.
+
+function removeUser(arr, username) {
+    let identifiedIdx = arr.findIndex(function (val) {
+        return val.username === username;
+    })
+    if (identifiedIdx === -1) return;
+
+    return arr.splice(identifiedIdx, 1)[0];
+};
